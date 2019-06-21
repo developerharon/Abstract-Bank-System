8c7f0b63b44b8e7b1d0fdb95c5784c9d36ee9aff
Haron Kipkorir <ronoharonkip@users.noreply.github.com>
2019-06-21 20:51:03 +0300

    Add project files.

---
+++ AbstractBankSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractBankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
